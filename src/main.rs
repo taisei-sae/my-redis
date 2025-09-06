8716f7b270f89f08fddef3aad7237d29c4ff10ec
taisei-sae <haisaijibunn010151@gmail.com>
2025-09-06 20:37:52 +0900

    WIP "Stared state"

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
-use mini_redis::{Command, Connection, Frame};
+use bytes::Bytes;
+use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+async fn process(socket: TcpStream, db: Db) {
+    // Why use statement here?
+    use mini_redis::Command;
+
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Command::Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Command::Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
@@ -30,10 +40,18 @@ async fn process(socket: TcpStream) {
 #[tokio::main]
 async fn main() {
     let lisner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _addr) = lisner.accept().await.unwrap();
-        tokio::spawn(async {
-            process(socket).await;
+        // clone db in order to move it to the new task
+        let db_clone = db.clone();
+        println!("Accepted");
+        tokio::spawn(async move {
+            process(socket, db_clone).await;
         });
     }
 }
