10dc33b803f1f768dfbdee32986d4d6240ece655
taisei-sae <haisaijibunn010151@gmail.com>
2025-09-17 00:00:10 +0900

    WIP

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
+
+#[tokio::main]
+async fn main() -> tokio::io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    let mut buf = [0; 10]; // compiler infers this type as u8
+
+    // read() reads the file source and copy into buf
+    let _ = f.read(&mut buf).await?;
+
+    println!("The bytes: {:?}", &buf);
+
+    let mut f = File::create("foo.txt").await.unwrap();
+
+    // write the bytes into file
+    let n = f.write(b"WATAWATA").await.unwrap();
+
+    println!("Wrote the first {} bytes of 'WATAWATA'.", n);
+    Ok(())
+}
